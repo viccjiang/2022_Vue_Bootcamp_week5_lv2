f2aa90f1820bb5fae5dddd97c41aec13898c187b
jiangvicc <binbinbb520@gmail.com>
2022-02-17 18:25:10 +0800

    前台CRUD

---
+++ javascript/app.js
@@ -0,0 +1,127 @@
+// 加入站點
+const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
+// 加入路徑
+const apiPath = 'jiangs2022vue3';
+
+const app = Vue.createApp({
+    data() {
+        return {
+            products:[],
+            cartData:{},
+            productId:'',
+            isLoadingItem:'',
+        };
+    },
+    methods:{
+        getProducts(){
+          axios.get(`${apiUrl}/api/${apiPath}/products/all`)
+            .then((res) => {
+              console.log(res);
+              this.products = res.data.products;
+              // console.log(this.products);
+            });
+        },
+        openProductModal(id){
+          this.productId = id;
+          this.$refs.productModal.openModal();
+        },
+        getCart(){
+          axios.get(`${apiUrl}/api/${apiPath}/cart`)
+          .then((res) => {
+            console.log(res);
+            this.cartData = res.data.data;
+            // console.log(this.cartData);
+          });
+        },
+        addToCart(id,qty = 1){
+          const data ={
+            product_id: id,
+            qty,
+          };
+          this.isLoadingItem = id;
+          axios.post(`${apiUrl}/api/${apiPath}/cart`,{data})
+            .then((res)=>{
+              console.log(res);
+              this.getCart();
+              this.$refs.productModal.closeModal();
+              this.isLoadingItem = '';
+            })
+        },
+        removeCartItem(id){
+          this.isLoadingItem = id;
+          axios.delete(`${apiUrl}/api/${apiPath}/cart/${id}`)
+            .then((res)=>{
+              console.log(res);
+              this.getCart();
+              this.isLoadingItem = '';
+            })
+        },
+        updateCartItem(item){
+          const data ={
+            product_id: item.id,
+            qty:item.qty,
+          };
+          this.isLoadingItem = item.id;
+          axios.put(`${apiUrl}/api/${apiPath}/cart/${item.id}`,{data})
+            .then((res)=>{
+              console.log(res);
+              this.getCart();
+              this.isLoadingItem = '';
+            })
+        },
+
+    },
+    mounted() {
+      this.getProducts();
+      this.getCart();
+      
+    },
+})
+
+// $refs
+app.component('product-modal',{
+  props : ['id'],
+  template:'#userProductModal',
+  data() {
+    return {
+      modal:{},
+      product:{},
+      qty: 1,
+    }
+  },
+  watch:{
+    id(){
+      this.getProduct();
+    },
+  },
+  methods: {
+    openModal(){
+      this.modal.show();
+    },
+    closeModal(){
+      this.modal.hide();
+    },
+    getProduct(){
+      axios.get(`${apiUrl}/api/${apiPath}/product/${this.id}`)
+      .then((res) => {
+        console.log(res);
+        this.product = res.data.product;
+        // console.log(this.products);
+      });
+    },
+    addToCart(){
+      this.$emit('add-cart',this.product.id,this.qty)
+    },
+    
+  },
+  mounted(){
+    // ref = "modal"
+    this.modal = new bootstrap.Modal(this.$refs.modal);
+  },
+});
+
+app.mount('#app');
+
+
+
+
